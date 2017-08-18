5c3d2f00ede97f5460d0b03a566bb73d33869e8b
Bruce Rabine <bruce_rabine@msn.com>
2017-08-18 07:46:32 -0500

    Trying to use ListGroup from react-bootstrap -- doesn't look good!

--- src/MemberList.js
+++ src/MemberList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-// import { ListGroup, ListGroupItem } from 'react-bootstrap';
+import { ListGroup, ListGroupItem } from 'react-bootstrap';
+import './App.css';
 
 
 class MemberList extends Component {
@@ -10,24 +11,61 @@ class MemberList extends Component {
     }
   }
 
-  componentDidMount() {
-    // console.log(`==>> MemberList props = ${JSON.stringify(this.props)}`);
-  }
+  selectUser = userId => () => {
+    alert(`>>> Selected userId = ${userId}`);
+
+  };
 
   render() {
     const members = this.state.members;
-    const memberList = members.map((user, index) => (
-      <li>
-        <h4>{user.name} <span>({user.userId})</span></h4>
-      </li>
+    const memberList = members.map(user => (
+      <ListGroupItem
+        id={`btn-${user.userId}`}
+        key={user.userId}
+        className="MemberListItem"
+        header={user.name}
+        bsStyle='link'
+        onClick={this.selectUser(user.userId)}>
+        ({user.userId})
+        {/* <h4>{user.name} <span>({user.userId})</span></h4> */}
+      </ListGroupItem>
     ));
 
     return (
-      <ul>
+      <ListGroup>
         {memberList}
-      </ul>
+      </ListGroup>
     );
   }
 }
 
 export default MemberList;
+
+
+// class MemberList extends Component {
+//   selectMember = handle => () => {
+//     this.props.history.push({ pathname: `/members/${handle}` });
+//   };
+//
+//   render() {
+//     const { members } = this.props;
+//
+//     const items = members.map(member =>
+//       (<ListItem key={member.handle} onClick={this.selectMember(member.handle)} button>
+//         <Avatar>
+//           <img src={member.imageUrl} alt="Member Avatar" width={40} height={40} />
+//         </Avatar>
+//         <ListItemText primary={member.name} secondary={member.handle} />
+//       </ListItem>),
+//     );
+//
+//     if (members && members.length) {
+//       return (
+//         <List>
+//           {items}
+//         </List>
+//       );
+//     }
+//     return null;
+//   }
+// }
